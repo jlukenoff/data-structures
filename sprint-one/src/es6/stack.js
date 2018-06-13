a6ec746d70171f23d033f13e26f71202f06305a2
John Lukenoff <jlukenoff@gmail.com>
2018-06-13 16:13:50 -0700

    implemented stack with stack style

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,27 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this;
+  }
+
+  push(value) {
+    let targetIndex = this.size();
+    this[targetIndex] = value;
+  }
+
+  pop() {
+    let targetIndex = this.size() - 1;
+    let targetValue = this[targetIndex];
+    delete this[targetIndex];
+    return targetValue;
+  }
+
+  size() {
+    let counter = 0;
+    for (let index in this) {
+      if (index.search(/\d/) !== -1) counter++;
+    }
+    return counter;
   }
 
 }
