1d30f386c406cf6558fc8b2857801115b374b409
jlukenoff <jlukenoff@gmail.com>
2018-06-14 13:45:15 -0700

    implement tree.insert

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,48 @@
 var BinarySearchTree = function(value) {
+  var tree = {};
+  tree.value = value;
+  
+  tree.insert = function(value, node) {
+    node = node || tree;
+    //check if value > node.value
+    if (value > node.value) {
+      //check if node.right 
+      if (node.right) {
+        //insert(value, node.right)
+        tree.insert(value, node.right);
+      } else {
+      // else node.right = {value: value};
+        node.right = {value: value};
+      }
+    //check if value < node.value
+    } else {     
+      //check if node.left 
+      if (node.left) {
+        //insert(value, node.left)
+        tree.insert(value, node.left)
+      } else {
+      // else node.left = {value: value};
+        node.left = {value: value};
+      }
+    }
+  }
+  
+  tree.contains = function(target, node) {
+    //check if target == node.value
+      return true;
+    //else if target > node.value and node.right !== undefined
+      // contains(target, node.right)
+    else 
+    
+  }
+  
+  tree.depthFirstLog = function(node) {
+    let logged = [];
+    
+    return logged;
+  }
+  
+  return tree;
 };
 
 
