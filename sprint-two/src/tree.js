c40de0e296e1d4d0a7d6e73257c31cc7e5a20e47
John Lukenoff <jlukenoff@gmail.com>
2018-06-14 14:26:07 -0700

    implement tree.addChild

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,11 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+
+  newTree.addChild = treeMethods.addChild;
+
+  newTree.contains = treeMethods.contains;
 
   return newTree;
 };
@@ -11,11 +15,34 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  //create new tree
+  let tree = Tree(value);
+  // push to current tree's children array
+  this.children.push(tree);
 };
 
-treeMethods.contains = function(target) {
+treeMethods.contains = function(target, node) {
+  node = node || this;
+  //check if node.value ==== target
+  if (node.value === target) {
+    return true;
+  }
+  //iterate through children
+  for (let i = 0; i < node.children.length; i++) {
+    //call contains on each child
+    return treeMethods.contains(target, node.children[i]);
+  }
+  return false;
 };
 
+let tree = Tree();
+tree.addChild(5);
+tree.addChild(6);
+tree/*?*/.children[0].addChild(7);
+tree.children[1].addChild(8);
+tree;//?
+tree.contains(8);//?
+tree.contains(7);//?
 
 
 /*
