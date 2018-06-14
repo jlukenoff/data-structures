769f598594f82fd67ae1872a7631c52f16d57244
jlukenoff <jlukenoff@gmail.com>
2018-06-14 10:47:43 -0700

    implement linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,44 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    //create new Node and set node.value to value
+    let node = Node(value);
+    //chcek if list is empty
+    if (list.head === null) {
+      //set list.head, list.tail to node
+      list.head = node;
+      list.tail = node;
+    } else {
+      //point tail.next to node
+      list.tail.next = node;
+      //set tail to node
+      list.tail = node;
+    }
   };
 
   list.removeHead = function() {
+    //store removed value for return
+    let removed = list.head.value;
+    //redirect head
+    list.head = list.head.next;
+    //return value
+    return removed;
   };
 
   list.contains = function(target) {
+    //declare initial current
+    let current = list.head;
+    //iterate through list 
+    while(current) {
+      // check if current.value === target
+      if (current.value === target) {
+        //return true;
+        return true;
+      }
+      //set current variable to current.next
+      current = current.next;
+    }
+    return false;
   };
 
   return list;
