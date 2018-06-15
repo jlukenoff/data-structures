bdf3474917da390e29491efe26afa9adc75ffe0c
jlukenoff <jlukenoff@gmail.com>
2018-06-15 10:23:27 -0700

    implement graph methods

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -17,6 +17,11 @@ Graph.prototype.contains = function(node) {
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  for (let key in this) {
+    if (key.search(/\d/) !== -1) {
+      this.removeEdge(this[key].value, node);
+    }
+  }
   delete this[node];
 };
 
@@ -32,6 +37,7 @@ Graph.prototype.hasEdge = function(fromNode, toNode) {
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
   this[fromNode].edges.push(toNode);
+  this[toNode].edges.push(fromNode);
 };
 
 // Remove an edge between any two specified (by value) nodes.
@@ -39,21 +45,28 @@ Graph.prototype.removeEdge = function(fromNode, toNode) {
   let edges = this[fromNode].edges;
   for (let i = 0; i < edges.length; i++) {
     if (edges[i] === toNode) {
-      edges.splice(i, 1);
+      this[fromNode].edges.splice(i, 1);
+      this[toNode].edges.splice(i, 1);
     }
   }
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  //iterate through graph
+  for (let node in this) {
+    if (node.search(/\d/) !== -1) {
+      cb(this[node].value)
+    };
+  }
 };
 
-let graph = new Graph();
-graph.addNode(5);
-graph.addNode(2);
-graph.addEdge(5, 2);
-graph.removeEdge(5, 2);
-graph.hasEdge(5, 2);//?
+// let graph = new Graph();
+// graph.addNode(5);
+// graph.addNode(2);
+// graph.addEdge(5, 2);
+// graph.removeEdge(5, 2);
+// graph.hasEdge(5, 2);//?
 /*
  * Complexity: What is the time complexity of the above functions?
  */
