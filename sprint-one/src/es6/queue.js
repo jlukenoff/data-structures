617ba8aa753587ec6ba0159f48cc98c4b93615c0
jlukenoff <jlukenoff@gmail.com>
2018-06-16 14:48:47 -0700

    made es6 queue more efficient

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,7 +2,7 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
-    this;
+    this._size = 0;
   }
 
   enqueue(value) {
@@ -12,22 +12,25 @@ class Queue {
       };
     }
     this['0'] = value;
+    this._size++;
   }
 
   dequeue() {
     let targetIndex = this.size() - 1;
     let targetValue = this[targetIndex];
     delete this[targetIndex];
+    this._size--;
     return targetValue;
 
   }
 
   size() {
-    let counter = 0;
+    return this._size < 0 ? 0 : this._size;
+    /*let counter = 0;
     for (let index in this) {
       if (index.search(/\d/) !== -1) counter++;
     }
-    return counter;
+    return counter;*/
   }
 
 }
